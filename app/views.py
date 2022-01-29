22db2fc5deb613e4d601acb1b7c2707c450bbeac
josephndegwa <kariukindegwa98@gmail.com>
2022-01-30 00:46:10 +0300

    add more categories

--- app/views.py
+++ app/views.py
@@ -12,8 +12,11 @@ def index():
     View root page function that returns the index page and its data
     '''
     popularity_headlines = get_headlines('popularity')
+    business_headlines = get_headlines('business')
+    technology_headlines = get_headlines('technology')
+    sports_headlines = get_headlines('sports')
     title = 'Welcome to Your News Page'
-    return render_template('index.html', title = title, popularity = popularity_headlines)
+    return render_template('index.html', title = title, popularity = popularity_headlines, business = business_headlines, technology = technology_headlines, sports = sports_headlines)
 
 
 @app.route('/headline/<int:headline_id>')
