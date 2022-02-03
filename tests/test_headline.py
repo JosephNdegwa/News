1b0d8eaebc44c7ca9b020140b5e2d395645232e5
josephndegwa <kariukindegwa98@gmail.com>
2022-02-03 10:42:42 +0300

    update test

--- tests/test_headline.py
+++ tests/test_headline.py
@@ -6,3 +6,13 @@ class HeadineTest(unittest.TestCase):
     '''
     Test Class to test the behaviour of the Headline class
     '''
+
+    def setup(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.headline_articles = Headline('UBS posts fall in quarterly profit to $1.35 billion, sets ambitious new targets - CNBC,Myanmars coup leaders tried to crush resistance. But one year on, its stronger than ever - CNN')
+
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.headline_articles, Headline))
